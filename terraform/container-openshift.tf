a813962f6fb712584417622df655d932365f86d0
Lionel Mace <lionel.mace@gmail.com>
2025-10-14 00:00:31 +0200

    removed odf

--- terraform/container-openshift.tf
+++ terraform/container-openshift.tf
@@ -227,20 +227,20 @@ resource "ibm_container_addons" "roks-addons" {
   # }
   # Source: https://github.com/IBM-Cloud/terraform-provider-ibm/tree/master/examples/openshift-data-foundation/addon/4.17.0
   # Specify workerpool to deploy ODF, if not specified ODF will deploy on all nodes
-  addons {
-    name            = "openshift-data-foundation"
-    version         = "4.17.0"
-    parameters_json = <<PARAMETERS_JSON
-    {
-        "billingType":"advanced"
-        "osdSize":"200Gi",
-        "numOfOsd":"2",
-        "osdStorageClassName":"ibmc-vpc-block-metro-10iops-tier",
-        "odfDeploy":"true"
-        "workerPools":"wpool-odf"
-    }
-    PARAMETERS_JSON
-  }
+  # addons {
+  #   name            = "openshift-data-foundation"
+  #   version         = "4.19.1"
+  #   parameters_json = <<PARAMETERS_JSON
+  #   {
+  #       "billingType":"advanced"
+  #       "osdSize":"200Gi",
+  #       "numOfOsd":"2",
+  #       "osdStorageClassName":"ibmc-vpc-block-metro-10iops-tier",
+  #       "odfDeploy":"true"
+  #       "workerPools":"wpool-odf"
+  #   }
+  #   PARAMETERS_JSON
+  # }
 }
 
 ## IAM
