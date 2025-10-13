56c8b7971eb7a995f12156391ced4f8ccd5970e3
Lionel Mace <lionel.mace@gmail.com>
2025-10-13 18:49:49 +0200

    removed monitoring

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -23,34 +23,37 @@ provider "ibm" {
   region           = var.region
 }
 
-provider "kubernetes" {
-  host                   = data.ibm_container_cluster_config.roks_cluster_config.host
-  token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-  cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
-  # config_path = "~/.kube/config" # Path to your kubeconfig file
-  # config_context = "your-context" # Optional: specify the kube context
-}
-
-# Helm to install IBM Cloud Logs
 ##############################################################################
-provider "helm" {
-  kubernetes {
-    host                   = data.ibm_container_cluster_config.roks_cluster_config.host
-    token                  = data.ibm_container_cluster_config.roks_cluster_config.token
-    cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
-  }
-  # IBM Cloud credentials are required to authenticate to the helm repo
-  registry {
-    url      = "oci://icr.io/ibm/observe/logs-agent-helm"
-    username = "iamapikey"
-    password = var.ibmcloud_api_key
-  }
-}
+# Temporarily commented out
 
-# Init cluster config for helm
-# ############################################################################
-data "ibm_container_cluster_config" "roks_cluster_config" {
-  # update this value with the cluster ID where these agents will be provisioned
-  cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
-  resource_group_id = ibm_resource_group.group.id
-}
+# provider "kubernetes" {
+#   host                   = data.ibm_container_cluster_config.roks_cluster_config.host
+#   token                  = data.ibm_container_cluster_config.roks_cluster_config.token
+#   cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+#   # config_path = "~/.kube/config" # Path to your kubeconfig file
+#   # config_context = "your-context" # Optional: specify the kube context
+# }
+
+# # Helm to install IBM Cloud Logs
+# ##############################################################################
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.ibm_container_cluster_config.roks_cluster_config.host
+#     token                  = data.ibm_container_cluster_config.roks_cluster_config.token
+#     cluster_ca_certificate = data.ibm_container_cluster_config.roks_cluster_config.ca_certificate
+#   }
+#   # IBM Cloud credentials are required to authenticate to the helm repo
+#   registry {
+#     url      = "oci://icr.io/ibm/observe/logs-agent-helm"
+#     username = "iamapikey"
+#     password = var.ibmcloud_api_key
+#   }
+# }
+
+# # Init cluster config for helm
+# # ############################################################################
+# data "ibm_container_cluster_config" "roks_cluster_config" {
+#   # update this value with the cluster ID where these agents will be provisioned
+#   cluster_name_id   = ibm_container_vpc_cluster.roks_cluster.id
+#   resource_group_id = ibm_resource_group.group.id
+# }
