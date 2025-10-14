c53b37dd670f281390efb43d78c022f6d1d9ca51
Lionel Mace <lionel.mace@gmail.com>
2025-10-14 17:46:06 +0200

    added cos bucket

---
+++ terraform/storage-cos-bucket.tf
@@ -0,0 +1,56 @@
+##############################################################################
+# COS Instance with 1 bucket
+##############################################################################
+
+# COS instance
+##############################################################################
+
+resource "ibm_resource_instance" "cos-instance" {
+  name              = format("%s-%s", local.basename, "cos-model")
+  service           = "cloud-object-storage"
+  plan              = "standard"
+  location          = "global"
+  resource_group_id = local.resource_group_id
+  tags              = var.tags
+
+  parameters = {
+    service-endpoints = "private"
+  }
+}
+
+## COS Bucket
+##############################################################################
+resource "ibm_cos_bucket" "bucket" {
+  bucket_name          = format("%s-%s", local.basename, "cos-bucket")
+  resource_instance_id = ibm_resource_instance.cos-instance.id
+  storage_class        = "smart"
+
+  cross_region_location = "eu"
+  endpoint_type         = "public"
+  #   endpoint_type = "private"
+}
+
+## HMAC Service Credentials
+##############################################################################
+resource "ibm_resource_key" "cos-hmac-backup" {
+  name                 = format("%s-%s", local.basename, "cos-instance-key")
+  resource_instance_id = ibm_resource_instance.cos-instance.id
+  role                 = "Writer"
+  parameters           = { HMAC = true }
+}
+
+locals {
+  backup-endpoints = [
+    {
+      name                  = "backup",
+      cos_access_key_id     = nonsensitive(ibm_resource_key.cos-hmac-backup.credentials["cos_hmac_keys.access_key_id"])
+      cos_secret_access_key = nonsensitive(ibm_resource_key.cos-hmac-backup.credentials["cos_hmac_keys.secret_access_key"])
+      cos_endpoint          = ibm_cos_bucket.backup-bucket.s3_endpoint_direct
+      cos_bucket_name       = ibm_cos_bucket.backup-bucket.bucket_name
+    }
+  ]
+}
+
+output "cos-instance-credentials" {
+  value = local.backup-endpoints
+}
