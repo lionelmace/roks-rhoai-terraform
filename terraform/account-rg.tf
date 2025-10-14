d9f8a2ca4b84d817872c0c75890e7b25e70dcae5
Lionel Mace <lionel.mace@gmail.com>
2025-10-14 12:00:58 +0200

    fix rg duplicate

--- terraform/account-rg.tf
+++ terraform/account-rg.tf
@@ -25,15 +25,3 @@ locals {
 # output "resource_group_name" {
 #   value = ibm_resource_group.group.name
 # }
-##############################################################################
-# Create a resource group or reuse an existing one
-##############################################################################
-
-resource "ibm_resource_group" "group" {
-  name = "${local.basename}-group"
-  tags = var.tags
-}
-
-output "resource_group_name" {
-  value = ibm_resource_group.group.name
-}
